bcfa6638fd580ffcd62299c51380eb1231f1ea16
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:51:47 +0800

    算出用餐總費用

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int total = 123;
+            Console.WriteLine("總金額為:" + total);
         }
     }
 }
